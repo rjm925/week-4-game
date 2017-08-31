876848587893a5e970f5a0d7d28b25ef0fb98500
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-08-31 17:52:49 -0400

    Done

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+var wins = 0;
+var losses = 0;
+var images = ["assets/images/purple.jpg", "assets/images/blue.jpg", "assets/images/yellow.jpg", "assets/images/green.jpg"];
+
+var target;
+var score;
+var crystalValues;
+
+newGame();
+
+function newGame () {
+	$("#totalWins").text(wins);
+	$("#totalLosses").text(losses);
+  target = Math.floor((Math.random() * 102) + 19);
+  $("#targetScore").text(target);
+  score = 0;
+  $("#userScore").text(score);
+  crystalValues = [getCrystalValue(), getCrystalValue(), getCrystalValue(), getCrystalValue()];
+  $("#crystals").html("");
+	setupCrystals();
+}
+
+function getCrystalValue() {
+  return Math.floor((Math.random() * 12) + 1);
+}
+
+function setupCrystals() {
+	for (var i = 0; i < crystalValues.length; i++) {
+		var image = $("<img>");
+		image.addClass("crystal-image");
+		image.attr("src", images[i]);
+		image.attr("data-value", crystalValues[i]);
+		$("#crystals").append(image);
+	}
+
+	$(".crystal-image").on("click", function() {
+		var crystalValue = ($(this).attr("data-value"));
+    crystalValue = parseInt(crystalValue);
+    score += crystalValue;
+    $("#userScore").text(score);
+
+    if (score === target) {
+      wins++;
+      $("#result").text("You Won!");
+      newGame();
+    }
+
+    else if (score >= target) {
+      losses++;
+      $("#result").text("You Lost.");
+      newGame();
+    }
+  });
+}
